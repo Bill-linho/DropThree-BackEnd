b98fb06a875f3c6a5b0272febf5c0cb4b8e351f2
Vitor Santos Pereirae <vitor_pereira153@estudante.sesisenai.org.br>
2025-11-18 23:43:29 -0300

    precisa atualizar

--- Serve.js
+++ Serve.js
@@ -1,16 +1,33 @@
-import Fastify from "fastify";
-import pg from "pg";
+import fastify from "fastify";
+import pkg from "pg";
 import cors from "@fastify/cors";
 
 const { Pool } = pkg;
 
 const pool = new Pool({
-    user: 'postgres',
+    user: 'local',
     host: 'localhost',
     database: 'DropThree',
+    password: '123',
     port: '5432'
 })
 
-const server = Fastify()
+const server = fastify()
 
-server.get('/usuario')
+server.post('/usuario', async (req, reply) => {
+    const { nome, email, telefone, senha_hash} = req.body;
+
+    try {
+        const response = await pool.query(
+            'INSERT INTO USUARIO ( nome, email, telefone, senha_hash) VALUES ($1, $2, $3, $4) RETURNING *',
+            [nome, email, telefone, senha_hash]
+        )
+        reply.status(200).send(response.rows)
+    } catch (e) {
+        reply.status(500).send({ error: e.message })
+    }
+})
+server.listen({
+    port: 3000,
+    host: '0.0.0.0'
+})
