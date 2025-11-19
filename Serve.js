ef0e8548a8b2b0aa25f2e6548753dce6732cf9ab
Bill-linho <seuemail@exemplo.com>
2025-11-18 22:20:15 -0300

    3

--- Serve.js
+++ Serve.js
@@ -1,3 +1,16 @@
 import Fastify from "fastify";
 import pg from "pg";
 import cors from "@fastify/cors";
+
+const { Pool } = pkg;
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'DropThree',
+    port: '5432'
+})
+
+const server = Fastify()
+
+server.get('/usuario')
